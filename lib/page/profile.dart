d6112548b40a714ef31c7d6961f24b033e5a4268
Imam Agus Faisal <81522796+iamfeizal@users.noreply.github.com>
2022-10-19 11:30:42 +0700

    penambahan fungsi

---
+++ lib/page/profile.dart
@@ -0,0 +1,81 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+
+class Profile extends StatefulWidget {
+  const Profile({Key? key}) : super(key: key);
+
+  @override
+  State<Profile> createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Help',
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.deepPurple,
+      ),
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                Text(
+                  "Data Diri",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 36,
+                  ),
+                ),
+                SizedBox(height: 40),
+                Image.asset(
+                  "assets/images/ImamAF.jpg",
+                  height: 100,
+                  width: 100,
+                  fit: BoxFit.fitWidth,
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Imam Agus Faisal',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontSize: 17,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  '124200077',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontSize: 17,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Klaten, 12 Desember 2001',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontSize: 17,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Bulutangkis',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontSize: 17,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    ));
+  }
+}
